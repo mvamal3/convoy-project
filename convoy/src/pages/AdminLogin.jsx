2b46656e1b052f8f39aa17f14bbbac7009c25faf
mvamal3 <techamal914@gmail.com>
2026-04-20 15:16:56 +0530

    error fix test

--- convoy/src/pages/AdminLogin.jsx
+++ convoy/src/pages/AdminLogin.jsx
@@ -6,7 +6,7 @@ import {
   CardContent,
   CardDescription,
   CardHeader,
-  CardTitle,...
+  CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
